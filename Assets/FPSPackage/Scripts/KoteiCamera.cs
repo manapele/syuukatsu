07bee4fb1011012179c259813f6806948f330cbd
Mana Ito <Mana Ito>
2016-06-25 13:46:14 +0900

    first commit

---
+++ Assets/FPSPackage/Scripts/KoteiCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KoteiCamera: MonoBehaviour {
+	public GameObject player;
+
+ void Start (){ 
+	}
+		
+	void Update(){
+		transform.position = player.transform.position + new Vector3 (0,5, 0);
+	}
+}
---
+++ Assets/FPSPackage/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	public int timeLimit = 20;
+	private Text text;
+
+	private float timeRemaining;
+	private bool timerStarted;
+
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		ResetTimer ();
+	}
+
+	public void ResetTimer(){
+		timeRemaining = timeLimit;
+		timerStarted = false;
+	}
+
+	public void StartTimer(){
+		timerStarted = true;
+	}
+
+		public void StopTimer(){
+			timerStarted = false;
+		}
+
+		public  float GetTimeRemaining(){
+		return timeRemaining;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (timerStarted) {
+			timeRemaining -= Time.deltaTime;
+			if (timeRemaining <= 0) {
+
+				timeRemaining = 0;
+				timerStarted = false;
+			}
+		}
+	
+		text.text = "Time:" + timeRemaining;
+	}
+}
