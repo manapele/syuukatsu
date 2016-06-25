fed02da9d69b2ba391bf9784082371532362eab6
Mana Ito <Mana Ito>
2016-06-25 16:42:51 +0900

    画面遷移

--- Assets/FPSPackage/Scripts/Timer.cs
+++ Assets/FPSPackage/Scripts/Timer.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
 	public int timeLimit = 20;
@@ -42,6 +43,8 @@ public class Timer : MonoBehaviour {
 
 				timeRemaining = 0;
 				timerStarted = false;
+
+				SceneManager.LoadScene ("TimeUp");
 			}
 		}
 	
